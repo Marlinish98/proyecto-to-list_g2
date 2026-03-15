3e7703b3291c03e2f6b72dd396090d4c31fbe118
Angelenamorado777 <albertoenamorado123@gmail.com>
2026-03-14 20:14:05 -0600

    añadiendo validaciones

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,23 +1,39 @@
-import { useState, type ChangeEvent } from "react"; 
+import { useState, type ChangeEvent } from "react";
 
+export const useForm = <T extends Record<string, unknown>>(initialState: T) => {
+  const [form, setForm] = useState<T>(initialState);
 
-export const useForm = <T>(initialState: T) => {
-   const [form, setForm] = useState<T>(initialState);
-   const handleChange = ({ target }: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-   const { name, value } = target;
+  const handleChange = ({ target }: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const { name, value } = target;
+     setForm({ ...form, [name]: value as T[keyof T] });
+  };
 
+  const handleDirectChange = <K extends keyof T>(name: K, value: T[K]) => {
     setForm({ ...form, [name]: value });
-    };
-    const handleDirectChange = <K extends keyof T>(name: K, value: T[K]) => {
-        setForm({ ...form, [name]: value });
-    };
+  };
 
-    const resetForm = () => setForm(initialState);
-    return {
-        ...form,  
-        form,             
-        handleChange,
-        handleDirectChange,
-        resetForm
-    };
+  const resetForm = () => setForm(initialState);
+  const validateBeforeSubmit = (): boolean => {
+  const nameValue = (form.name as string) || "";
+  const dateValue = form.date;
+
+    if (nameValue.trim() === "") {
+     alert("El nombre no puede estar vacío");
+     return false;
+    }
+
+    if (!dateValue) {
+      alert("Selecciona una fecha en el calendario");
+      return false;
+    }
+    return true;};
+
+  return {
+    ...form,
+    form,
+    handleChange,
+    handleDirectChange,
+    resetForm,
+    validateBeforeSubmit
+  };
 };
